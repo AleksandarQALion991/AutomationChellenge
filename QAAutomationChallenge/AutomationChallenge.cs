cd3b85400e419381c5d6867c836567e59b7b82c3
Aleksandar_Saric <alsa1705@enjoying.rs>
2020-12-21 20:09:06 +0100

    System Property changes

--- QAAutomationChallenge/AutomationChallenge.cs
+++ QAAutomationChallenge/AutomationChallenge.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using log4net;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using QAAutomationChallenge.Extensions;
 
@@ -7,6 +10,21 @@ namespace QAAutomationChallenge
     [TestClass]
     public class AutomationChallenge
     {
+        [AssemblyInitialize]
+        public static void Configure(TestContext testContext)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            var fi = new FileInfo("log4net.config");
+            log4net.Config.XmlConfigurator.Configure(logRepository, fi);
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            var compName = Environment.GetEnvironmentVariable("COMPUTERNAME");
+            var user = Environment.GetEnvironmentVariable("USER");
+        }
+
         [TestMethod]
         public void TestChromeWindows()
         {
